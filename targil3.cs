4a3301319143dedab69bf423d61072dd8259453d
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-02 20:33:20 +0200

    Create targil3.cs

---
+++ targil3.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace print1to10
+{
+	class Program
+{
+static void Main(string[] args)
+		{
+                int a;
+                a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+              printNumber1To10(a);
+              // functions 
+ }
+
+
+ private static void printNumber1To10(int a)
+            {
+                case 1: Console.WriteLine("one");
+                break;
+                case 2: Console.WriteLine("two");
+                break;
+                case 3: Console.WriteLine("three");
+                break;
+                case 4: Console.WriteLine("four");
+                break;
+                case 5: Console.WriteLine("five");
+                break;
+                case 6: Console.WriteLine("six");
+                break;
+                case 7: Console.WriteLine("seven");
+                break;
+                case 8: Console.WriteLine("eight");
+                break;
+                case 9: Console.WriteLine("nine");
+                break;
+                case 10:Console.WriteLine("ten");
+                break;
+                 default:
+                    Console.WriteLine("number not in range");
+                    break;
+            }
+
+            Thread.Sleep(500);
+            }
+            }
+}            
